b1b21db808dde8e871a0667b228bd45ad51972d1
Fernando Herrera <fernando.herrera85@gmail.com>
2018-09-20 12:32:08 -0600

    ios support

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v5';
+const STATIC_CACHE    = 'static-v6';
 const DYNAMIC_CACHE   = 'dynamic-V1';
 const INMUTABLE_CACHE = 'inmutable-V1';
 
