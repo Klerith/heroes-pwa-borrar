a746f85cc1c63ca441cc566bd0e7f51d995741c6
Fernando Herrera <fernando.herrera85@gmail.com>
2018-09-20 10:40:14 -0600

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,17 @@
+
+// Funciones para ayudar al SW
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+    if ( res.ok ) {
+        
+        return caches.open( dynamicCache ).then( cache => {
+    
+            cache.put( req, res.clone() );
+    
+            return res;
+        });
+
+    }
+
+
+}
